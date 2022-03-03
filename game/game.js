fad038b2dd7e7ccc819e0f621d85da83f4b91253
Rai Phairow <rai.phairow@pubnub.com>
2022-03-02 21:09:06 -0800

    move square accross the board

--- game/game.js
+++ game/game.js
@@ -47,7 +47,7 @@ class Game {
       elapsed += delta;
       // Update the sprite's X position based on the cosine of our elapsed time.  We divide
       // by 50 to slow the animation down a bit...
-      sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+      sprite.x = 300.0 + Math.cos(elapsed/50.0) * 300.0;
     });
   }
 }
